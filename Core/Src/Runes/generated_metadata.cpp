f0552bb2dfe898db26adb601f9ea4dce23c1606d
Jorge <jorgecaru3@gmail.com>
2025-10-18 00:47:13 +0200

    feat: pin configurado e iniciado

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
         "****************"  // placeholder for beggining
-        "20250605T165128"   // DateTime using ISO-8601 format
+        "20251018T004240"   // DateTime using ISO-8601 format
         " "                 // alignment
-        "77b804dd"          // STLIB commit
+        "1af648a0"          // STLIB commit
         "--------"          // ADJ commit
-        "3a365135"          // Board commit
+        "0307d467"          // Board commit
         // the '=' is used for unparsing
         ;
 }
