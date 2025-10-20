69774da6ab18f49f375d03b8befefc26b47a0969
Jorge <jorgecaru3@gmail.com>
2025-10-20 19:31:46 +0200

    feat: funcional, pero no entiendo algunas cosas

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -11,13 +11,20 @@ int main(void) {
     SharedMemory::start();
 #endif
 
+    // Esto habilita todos los RCC de los gpio?
     DigitalOutput yellow_led(PE1);
-    Pin::start();
-    //COmentario de prueba
+    //Pin::start();
+
+    STLIB::start();
+    //Time::start();
+    Time::register_low_precision_alarm(1000, [&]() { yellow_led.toggle(); });
+
+    while(1){
+        STLIB::update();
+    }
 }
 
 void Error_Handler(void) {
     ErrorHandler("HAL error handler triggered");
-    while (1) {
-    }
+    while (1) {    }
 }
---
+++ out/build/nucleo-debug/CMakeFiles/3.28.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.14.0-33-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.14.0-33-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/jorge/hyperloop/blinking_led_STM32/toolchains/stm32.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "ARM")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
